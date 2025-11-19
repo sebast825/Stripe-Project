bb5db7f7a4cc8c5e50f943010a81e34e5da7c848
sebast825 <sebamolina825@gmail.com>
2025-11-19 20:11:34 -0300

    Add IUserSubscriptionService with AddAsync method
    - Created IUserSubscriptionService interface
    - Added AddAsync(UserSubscription)

---
+++ back/Aplication/Interfaces/Services/IUserSubscriptionService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Dto.UserSubscription;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Interfaces.Services
+{
+    public interface IUserSubscriptionService
+    {
+        Task<UserSubscriptionResponseDto> AddAsync(UserSubscription userSubscription);
+    }
+}
---
+++ back/Core/Dto/UserSubscription/UserSubscriptionResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dto.UserSubscription
+{
+    public class UserSubscriptionResponseDto
+    {
+        public int Id { get; set; }
+        public string Plan { get; set; }
+        public DateTime CurrentPeriodEnd { get; set; }
+        public string Status { get; set; }
+    }
+}
