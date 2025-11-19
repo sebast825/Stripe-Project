ac24ca4f7d8c6cbf4e4d1cf634199e4544d562ed
sebast825 <sebamolina825@gmail.com>
2025-11-19 20:04:57 -0300

    added UserSubscriptionRepository
    implemented interface IUserSubscriptionRepository

---
+++ back/Infrastructure/Repositories/UserSubscriptionRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UserSubscriptionRepository : IUserSubscriptionRepository
+    {
+        private readonly DataContext _dataContext;
+        public UserSubscriptionRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task AddAsync(UserSubscription userSubscriptions)
+        {
+            await _dataContext.Set<UserSubscription>().AddAsync(userSubscriptions);
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
