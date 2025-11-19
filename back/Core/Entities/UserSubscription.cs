438ec9145ac721983ecdfce718b8086b5675060a
sebast825 <sebamolina825@gmail.com>
2025-11-19 20:03:11 -0300

    Add IUserSubscriptionRepository.AddAsync and UserSubscription entity
    - Added AddAsync method to IUserSubscriptionRepository
    - Renamed entity to singular (UserSubscription)
    - Added UserSubscription entity attributes

---
+++ back/Core/Entities/UserSubscription.cs
@@ -0,0 +1,25 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class UserSubscription : ClassBase
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public string StripeSubscriptionId { get; set; }
+        public string StripeCustomerId { get; set; }
+
+        public SubscriptionPlanType Plan { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime CurrentPeriodEnd { get; set; }
+
+        public SubscriptionStatus Status { get; set; }
+    }
+}
--- back/Core/Interfaces/Repositories/IUserSubscriptionRepository.cs
+++ back/Core/Interfaces/Repositories/IUserSubscriptionRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entities
+namespace Core.Interfaces.Repositories
 {
-    internal class UserSubscriptions
+    internal interface ISubscriptionRepository
     {
     }
 }
