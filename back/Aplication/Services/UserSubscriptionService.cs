c504ac75a504a5076988d31055ef612af4610981
sebast825 <sebamolina825@gmail.com>
2025-11-19 20:22:03 -0300

    implemented interface IUserSubscriptionService
    implemented AddAsync method

---
+++ back/Aplication/Services/UserSubscriptionService.cs
@@ -0,0 +1,36 @@
+﻿using Aplication.Interfaces.Services;
+using Core.Dto.UserSubscription;
+using Core.Entities;
+using Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Services
+{
+    internal class UserSubscriptionService : IUserSubscriptionService
+    {
+        private readonly IUserSubscriptionRepository _userSubscriptionRepository;
+        public UserSubscriptionService(IUserSubscriptionRepository userSubscriptionRepository)
+        {
+            _userSubscriptionRepository = userSubscriptionRepository;
+        }
+
+        public async Task<UserSubscriptionResponseDto> AddAsync(UserSubscription userSubscription)
+        {
+            UserSubscription rsta = await _userSubscriptionRepository.AddAsync(userSubscription);
+
+            return new UserSubscriptionResponseDto
+            {
+                Id = rsta.Id,
+                Plan = rsta.Plan.ToString(),
+                CurrentPeriodEnd = rsta.CurrentPeriodEnd,
+                Status = rsta.Status.ToString()
+            };
+
+   
+        }
+    }
+}
