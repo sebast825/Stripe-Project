21820b97e0b155283f0bfadf764d3c9fe71178c2
sebast825 <sebamolina825@gmail.com>
2025-11-19 16:51:07 -0300

    added IStripePaymentService

---
+++ back/Aplication/Interfaces/Payments/IStripePaymentService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Interfaces.Payments
+{
+    public interface IStripePaymentService
+    {
+        Task<string> CreateSubscriptionAsync(int userId, SubscriptionPlanType plan);
+        Task CancelSubscriptionAsync(string stripeSubscriptionId);
+        Task HandleWebhookAsync(string json, string signature);
+        Task<string> CreateCustomerIfNotExistsAsync(int userId);
+
+    }
+
+}
