bb4f97b6489128b8a368bc09bd38b46e660bc343
sebast825 <sebamolina825@gmail.com>
2025-11-19 19:52:48 -0300

    added StripePaymentService
    StripePaymentService implement interface IStripePaymentService
    added aplication in csproj

---
+++ back/Infrastructure/Payments/StripePaymentService .cs
@@ -0,0 +1,33 @@
+﻿using Aplication.Interfaces.Payments;
+using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Payments
+{
+    public class StripePaymentService : IStripePaymentService
+    {
+        public Task CancelSubscriptionAsync(string stripeSubscriptionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> CreateCustomerIfNotExistsAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> CreateSubscriptionAsync(int userId, SubscriptionPlanType plan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleWebhookAsync(string json, string signature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
