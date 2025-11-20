ed12425570507c27bd899ddfeab509821bb4a7c9
sebast825 <sebamolina825@gmail.com>
2025-11-20 10:13:39 -0300

    added entity SubscriptionPlan

---
+++ back/Core/Entities/SubscriptionPlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class SubscriptionPlan : ClassBase
+    {
+        public string Name { get; set; } 
+        public string StripePriceId { get; set; } //
+        public decimal Price { get; set; }
+        public string Interval { get; set; } // daily, monthly, yearly
+    }
+}
