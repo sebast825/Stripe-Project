b6c3a2aeab209477d6cb4382883a3bbceb746334
sebast825 <sebamolina825@gmail.com>
2025-11-19 21:01:22 -0300

    added irepository and repository
    implemented repository

---
+++ back/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Interfaces.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DataContext __dataContext;
+
+        public Repository(DataContext _dataContext)
+        {
+            __dataContext = _dataContext;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+            => await __dataContext.Set<T>().FindAsync(id);
+
+        public async Task<IReadOnlyList<T>> ListAsync()
+            => await __dataContext.Set<T>().ToListAsync();
+
+        public async Task AddAsync(T entity)
+        {
+            __dataContext.Set<T>().Add(entity);
+            await __dataContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            __dataContext.Set<T>().Update(entity);
+            await __dataContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            __dataContext.Set<T>().Remove(entity);
+            await __dataContext.SaveChangesAsync();
+        }
+    }
+
+}
