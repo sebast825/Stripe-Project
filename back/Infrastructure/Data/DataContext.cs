85f5ba15b93a57a8c0cb442a5c28955b191b4b9d
sebast825 <sebamolina825@gmail.com>
2025-11-20 14:26:08 -0300

    added table UserSubscriptions

--- back/Infrastructure/Data/DataContext.cs
+++ back/Infrastructure/Data/DataContext.cs
@@ -12,9 +12,14 @@ namespace Infrastructure.Data
     {
         public DataContext(DbContextOptions<DataContext> options) :base(options) { }
 
+        //Auth
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<UserLoginHistory> UserLoginHistory { get; set; } = null!;
         public DbSet<RefreshToken>RefreshTokens { get; set; } = null!;
         public DbSet<SecurityLoginAttempt> SecurityLoginAttempts { get; set; } = null!;
+
+        //Payments
+        public DbSet<UserSubscription> UserSubscriptions { get; set; } = null!;
+
     }
 }
