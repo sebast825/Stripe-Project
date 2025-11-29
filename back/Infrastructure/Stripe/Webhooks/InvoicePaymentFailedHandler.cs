d46d705552689a6863b5efa75812566c6527a526
sebast825 <sebamolina825@gmail.com>
2025-11-29 17:24:07 -0300

    added InvoicePaymentFailedHandler

---
+++ back/Infrastructure/Stripe/Webhooks/InvoicePaymentFailedHandler.cs
@@ -0,0 +1,35 @@
+﻿using Aplication.Dto;
+using Aplication.Helpers;
+using Aplication.Interfaces.Services;
+using Aplication.Interfaces.Stripe;
+using Core.Dto.UserSubscription;
+using Core.Entities;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Stripe.Webhooks
+{
+    public class InvoicePaymentFailedHandler : IStripeWebhookHandler
+    {
+        public string EventType => "invoice.payment_failed";
+        private readonly ISubscriptionPaymentRecordService _subscriptionPaymentRecordService;
+
+        public InvoicePaymentFailedHandler(ISubscriptionPaymentRecordService subscriptionPaymentRecordService)
+        {
+            _subscriptionPaymentRecordService = subscriptionPaymentRecordService;
+        }
+        public async Task HandleAsync(Event stripeEvent)
+        {
+
+            var invoice = stripeEvent.Data.Object as Invoice;
+            await _subscriptionPaymentRecordService.AddAsync(invoice);
+
+        }
+       
+    }
+}
