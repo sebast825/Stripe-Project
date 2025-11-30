612142b2dd6a5a64b123275641061166f9882767
sebast825 <sebamolina825@gmail.com>
2025-11-29 22:58:43 -0300

    feat(user): added roles

--- back/Core/Entities/User.cs
+++ back/Core/Entities/User.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Constants;
+using Core.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,23 +12,20 @@ namespace Core.Entities
     public class User : ClassBase
     {
 
-    
+
 
         public required string Email { get; set; }
         public required string Password { get; set; }
 
         public string? FullName { get; set; }
         public string? StripeCustomerId { get; set; }
-
-        
+        public UserRole Role { get; set; } = UserRole.User;
 
         public ICollection<UserLoginHistory> LoginAttempts { get; set; } = new List<UserLoginHistory>();
         public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
 
-
         public void Validate()
         {
-
             ValidteEmail();
             ValidatePassword();
         }
@@ -43,7 +41,6 @@ namespace Core.Entities
             try
             {
                 var addr = new System.Net.Mail.MailAddress(Email);
-                
             }
             catch
             {
