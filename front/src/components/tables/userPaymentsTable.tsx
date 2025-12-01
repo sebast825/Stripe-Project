4230857340511e0dee4d80992eb9d5e27779e0d5
sebast825 <sebamolina825@gmail.com>
2025-11-30 22:51:21 -0300

    refactor: create reusable Table component from userDetailPage
    - Extract and combine dashboard components into generic Table
    - Table features: pagination

---
+++ front/src/components/tables/userPaymentsTable.tsx
@@ -0,0 +1,92 @@
+import { Table } from "react-bootstrap";
+import { useGetSubscriptionPayments } from "../../hooks/subscription/useGetSubscriptionPayments";
+import { usePagination } from "../../hooks/usePagination";
+import type { SubscriptionPaymentDto } from "../../types/SubscriptionPaymentDto.types";
+import PaginationBtns from "../paginationBtns";
+import { CardMessage } from "../cardMessage";
+
+export function UserPaymentsTable({ id }: { id: number }) {
+  const { page, pageSize, goToPage } = usePagination();
+
+  const { data: payments } = useGetSubscriptionPayments(id, page, pageSize);
+  return (
+    <>
+      {payments?.totalItems === 0 ? (
+        <CardMessage
+          type="warning"
+          message="El usuario no tiene facturas disponibles"
+        />
+      ) : (
+        <Table striped bordered hover style={{ overflowX: "auto" }}>
+          <thead>
+            <tr className="text-center">
+              <th>Monto</th>
+              <th>Moneda</th>
+              <th>Estado</th>
+              <th>Fecha de Pago</th>
+              <th>Periodo</th>
+              <th>Factura</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments?.data
+              ?.filter(
+                (payment): payment is SubscriptionPaymentDto => payment !== null
+              )
+              .map((payment, index) => (
+                <tr key={index} className="text-center">
+                  <td>${(payment.amountPaid / 100).toFixed(2)}</td>
+                  <td>{payment.currency}</td>
+                  <td>
+                    <span
+                      className={`badge ${
+                        payment.status === "paid"
+                          ? "bg-success"
+                          : payment.status === "pending"
+                          ? "bg-warning"
+                          : payment.status === "failed"
+                          ? "bg-danger"
+                          : "bg-secondary"
+                      }
+                      p-2
+                      `}
+                    >
+                      {payment.status}
+                    </span>
+                  </td>
+                  <td>
+                    {payment.paidAt
+                      ? new Date(payment.paidAt).toLocaleDateString()
+                      : "No pagado"}
+                  </td>
+                  <td>
+                    {new Date(payment.periodStart).toLocaleDateString()} -{" "}
+                    {new Date(payment.periodEnd).toLocaleDateString()}
+                  </td>
+                  <td>
+                    {payment.invoiceUrl ? (
+                      <a
+                        href={payment.invoiceUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-sm btn-outline-primary"
+                      >
+                        Ver Factura
+                      </a>
+                    ) : (
+                      "N/A"
+                    )}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      )}
+      <PaginationBtns
+        page={page}
+        totalPages={payments?.totalPages!}
+        goToPage={goToPage}
+      />
+    </>
+  );
+}
--- front/src/pages/admin/userDetailPage.tsx
+++ front/src/pages/admin/userDetailPage.tsx
@@ -1,22 +1,12 @@
-import { useParams } from "react-router-dom";
-import { useGetSubscriptionPayments } from "../../hooks/subscription/useGetSubscriptionPayments";
-import { Table } from "react-bootstrap";
-import type { SubscriptionPaymentDto } from "../../types/SubscriptionPaymentDto.types";
-import { useLocation } from "react-router-dom";
-import { usePagination } from "../../hooks/usePagination";
-import PaginationBtns from "../../components/paginationBtns";
+import { useParams ,useLocation} from "react-router-dom";
+import { UserPaymentsTable } from "../../components/tables/userPaymentsTable";
 
 export const UserDetailPage = () => {
   const location = useLocation();
   const { id } = useParams<{ id: string }>();
-  const { page, pageSize, goToPage } = usePagination();
   const userName = location.state?.userName || "NameEmpty";
 
-  const { data: payments } = useGetSubscriptionPayments(
-    Number(id),
-    page,
-    pageSize
-  );
+
   return (
     <>
       <div className=" margin-top  d-flex flex-column justify-content-center align-items-center w-100 p-3 p-sm-5 ">
@@ -24,75 +14,7 @@ export const UserDetailPage = () => {
           <h1 className="fw-bold">Detalles de {userName}!</h1>
           <p>Panel Administrador</p>
         </div>
-        <Table striped bordered hover style={{ overflowX: "auto" }}>
-          <thead>
-            <tr className="text-center">
-              <th>Monto</th>
-              <th>Moneda</th>
-              <th>Estado</th>
-              <th>Fecha de Pago</th>
-              <th>Periodo</th>
-              <th>Factura</th>
-            </tr>
-          </thead>
-          <tbody>
-            {payments?.data
-              ?.filter(
-                (payment): payment is SubscriptionPaymentDto => payment !== null
-              )
-              .map((payment, index) => (
-                <tr key={index} className="text-center">
-                  <td>${(payment.amountPaid / 100).toFixed(2)}</td>
-                  <td>{payment.currency}</td>
-                  <td>
-                    <span
-                      className={`badge ${
-                        payment.status === "paid"
-                          ? "bg-success"
-                          : payment.status === "pending"
-                          ? "bg-warning"
-                          : payment.status === "failed"
-                          ? "bg-danger"
-                          : "bg-secondary"
-                      }
-                      p-2
-                      `}
-                    >
-                      {payment.status}
-                    </span>
-                  </td>
-                  <td>
-                    {payment.paidAt
-                      ? new Date(payment.paidAt).toLocaleDateString()
-                      : "No pagado"}
-                  </td>
-                  <td>
-                    {new Date(payment.periodStart).toLocaleDateString()} -{" "}
-                    {new Date(payment.periodEnd).toLocaleDateString()}
-                  </td>
-                  <td>
-                    {payment.invoiceUrl ? (
-                      <a
-                        href={payment.invoiceUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="btn btn-sm btn-outline-primary"
-                      >
-                        Ver Factura
-                      </a>
-                    ) : (
-                      "N/A"
-                    )}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-        <PaginationBtns
-          page={page}
-          totalPages={payments?.totalPages!}
-          goToPage={goToPage}
-        />
+        <UserPaymentsTable id={Number(id)}/>
       </div>
     </>
   );
