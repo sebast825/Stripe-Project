a1022af8ae0f65d13f8cf9a4ca7edc8c7f0eae5f
sebast825 <sebamolina825@gmail.com>
2025-12-03 14:31:45 -0300

    feat: prevent redirect to login when no credentials are required

--- front/src/hooks/auth/useAuthRedirect.ts
+++ front/src/hooks/auth/useAuthRedirect.ts
@@ -2,15 +2,17 @@
 import { useEffect } from "react";
 import { useAuthStore } from "../../states/auth/auth.store";
 import { useRedirect } from "../useRedirect";
+import { useLocation } from "react-router-dom";
 
 // Redirects to login when the user becomes unauthenticated.
 // Prevents infinite retry loops after a 401 response.
 export function useAuthRedirect() {
    const {goToLogin} = useRedirect();
+   const {pathname} = useLocation();
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
-
+  const homePath = "/home"
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (!isAuthenticated && pathname != homePath) {
       goToLogin();
     }
   }, [isAuthenticated]);
