8d9217019155ee0b09f38b6f50a698dc22ce8ece
sebast825 <sebamolina825@gmail.com>
2025-12-02 23:27:46 -0300

    feat: show info message when user already has active subscription

--- front/src/hooks/subscription/useCreateSubscriptionCheckout.ts
+++ front/src/hooks/subscription/useCreateSubscriptionCheckout.ts
@@ -3,7 +3,7 @@ import apiClient from "../../api/client";
 import useToastit from "../useToastit";
 
 export const useCreateSubscriptionCheckout = () => {
-  const { error } = useToastit();
+  const { info } = useToastit();
   return useMutation({
     mutationFn: async (planId: number) => {
       console.log(planId);
@@ -11,7 +11,11 @@ export const useCreateSubscriptionCheckout = () => {
       return res.data;
     },
     onError: (err: any) => {
-      error(err.message);
+      if (err.data.Error == "ValidationException") {
+        info(
+          "Ya tenés una subscripción activa. Podés cambiar de plan desde 'Gestionar Plan' en el dashboard"          
+        );
+      }
     },
   });
 };
