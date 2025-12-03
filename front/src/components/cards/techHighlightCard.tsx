4102939b9edb91db7ee6cfa2601ba606744bb15a
sebast825 <sebamolina825@gmail.com>
2025-12-03 14:38:51 -0300

    refactor(styles): added border color

--- front/src/components/cards/techHighlightCard.tsx
+++ front/src/components/cards/techHighlightCard.tsx
@@ -1,21 +1,18 @@
 import { Col } from "react-bootstrap";
 
 export const TechHighlightCard = ({
-   icon,
+  icon,
   title,
   text,
 }: {
-   icon?: string;
+  icon?: string;
   title: string;
   text: string;
 }) => {
   return (
     <Col md={4}>
-      <div className="p-4 border rounded-4  h-100 shadow-sm">
-      {
-   icon
- &&       <div className="fs-2 mb-2">{icon}</div>
- } 
+      <div className="p-4 border rounded-4 border-primary  h-100 shadow-sm">
+        {icon && <div className="fs-2 mb-2">{icon}</div>}
         <h5 className="fw-bold">{title}</h5>
         <p className="text-muted small mb-0">{text}</p>
       </div>
