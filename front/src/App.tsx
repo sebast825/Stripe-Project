3cf77e8207064afedec4d24341873e68d18cbd47
sebast825 <sebamolina825@gmail.com>
2025-12-05 19:02:11 -0300

    feat: added boostwatch styles

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,21 @@
 import "./App.css";
 import { AppRoutes } from "./routes/appRoutes";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/navbar";
 import { useAuthRedirect } from "./hooks/auth/useAuthRedirect";
 import { ToastContainer, Flip } from "react-toastify";
-
+import "bootswatch/dist/united/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   useAuthRedirect();
   return (
     <>
-      <div className=" d-flex flex-column justify-content-start align-items-start vw-100 vh-100 ">
+      <div className=" d-flex flex-column justify-content-start align-items-start vw-100 min-vh-100 ">
         <NavBar />
         <AppRoutes />
+
         <ToastContainer
           position="top-right"
-          autoClose={3000} 
+          autoClose={3000}
           hideProgressBar={false}
           newestOnTop={true}
           closeOnClick
