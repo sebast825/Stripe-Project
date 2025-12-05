6fbda7b1930e0539b6c6d882ff668385dff0b197
sebast825 <sebamolina825@gmail.com>
2025-12-05 19:47:50 -0300

    feat: added white space css to actionstatusPage and updated text for success page

--- front/src/components/actionStatusPage.tsx
+++ front/src/components/actionStatusPage.tsx
@@ -5,7 +5,7 @@ export const ActionStatusPage= ({title,text,variant}:{title:string,text:string,v
   <Container className="d-flex justify-content-center align-items-center vh-100">
       <Card className={`p-5 text-center shadow-sm border-${variant}`}>
         <h1 className={`mb-3 text-${variant}`}>{title}</h1>
-        <h6 className="text-muted mb-4">
+        <h6 className="text-muted mb-4" style={{ whiteSpace: 'pre-line' }}>
           {text}
         </h6>
         <Button variant="secondary"  onClick={() => window.close()}>
--- front/src/pages/actions/successPage.tsx
+++ front/src/pages/actions/successPage.tsx
@@ -1,12 +1,12 @@
-import { Container, Card, Button } from "react-bootstrap";
 import { ActionStatusPage } from "../../components/actionStatusPage";
 
 export default function SuccessActionPage() {
   return (
-    <ActionStatusPage title="Acción completada" text="La operación se realizó correctamente." variant="success" />
-    
-    
-    
- 
+    <ActionStatusPage
+      title="Acción completada"
+      text={`La operación se realizó correctamente.
+Cuando terminemos de procesar la solicitud verás tu subscripción en el panel de usuarios.`}
+      variant="success"
+    />
   );
 }
